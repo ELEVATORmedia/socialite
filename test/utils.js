f3f567f9173fcccf6e81edc5e7fa6a3c6b0f7ff0
Jesse Meza <jmeza081@gmail.com>
2020-02-05 14:20:26 -0600

    add custom youtube url tests

--- test/utils.js
+++ test/utils.js
@@ -1,4 +1,4 @@
-const { TEST_TYPES } = require('./enums');
+const { STANDARD_TEST_TYPES, YOUTUBE_ONLY_TEST_TYPES } = require('./enums');
 
 /**
  * Utility function to retrieve a standardized description for tests
@@ -13,14 +13,14 @@ const getDescription = ({ arg, expected }) => {
  *
  *
  * @param {*} site e.g., "facebook"
- * @param {*} type  - enumaration of type see TEST_TYPES
+ * @param {*} type  - enumaration of type see STANDARD_TEST_TYPES
  * @param {*} defaultUser  - expected user to use in test case.
  */
 const getExtractUsernameTestCase = (site, type, defaultUser = 'myUser') => {
     const tests = [];
 
     switch (type) {
-        case TEST_TYPES.ABSOLUTE_URLS:
+        case STANDARD_TEST_TYPES.ABSOLUTE_URLS:
             tests.push({
                 arg: `https://www.${site}.com/${defaultUser}`,
                 expected: defaultUser,
@@ -30,13 +30,13 @@ const getExtractUsernameTestCase = (site, type, defaultUser = 'myUser') => {
                 expected: defaultUser,
             });
             break;
-        case TEST_TYPES.NO_PROTOCOL:
+        case STANDARD_TEST_TYPES.NO_PROTOCOL:
             tests.push({
                 arg: `www.${site}.com/${defaultUser}`,
                 expected: defaultUser,
             });
             break;
-        case TEST_TYPES.NO_WWW:
+        case STANDARD_TEST_TYPES.NO_WWW:
             tests.push({
                 arg: `https://${site}.com/${defaultUser}`,
                 expected: defaultUser,
@@ -46,39 +46,93 @@ const getExtractUsernameTestCase = (site, type, defaultUser = 'myUser') => {
                 expected: defaultUser,
             });
             break;
-        case TEST_TYPES.NAKED_DOMAIN:
+        case STANDARD_TEST_TYPES.NAKED_DOMAIN:
             tests.push({
                 arg: `${site}.com/${defaultUser}`,
                 expected: defaultUser,
             });
             break;
-        case TEST_TYPES.NO_INPUT:
+        case STANDARD_TEST_TYPES.NO_INPUT:
             tests.push({
                 arg: '',
                 expected: '',
             });
             break;
-        case TEST_TYPES.SPECIAL_CHARS:
+        case STANDARD_TEST_TYPES.SPECIAL_CHARS:
             tests.push({
                 arg: `${site}.com/@${defaultUser}`,
                 expected: defaultUser,
             });
             break;
-        case TEST_TYPES.WRONG_DOMAIN:
+        case STANDARD_TEST_TYPES.WRONG_DOMAIN:
             tests.push({
                 arg: `https://www.totallylegit.com/${defaultUser}`,
                 expected: defaultUser,
             });
             break;
-        case TEST_TYPES.USERNAME_ONLY:
+        case STANDARD_TEST_TYPES.USERNAME_ONLY:
             tests.push({
                 arg: defaultUser,
                 expected: defaultUser,
             });
             break;
-        case TEST_TYPES.SPACES:
+        case STANDARD_TEST_TYPES.SPACES:
             tests.push({
                 arg: `https://w ww. ${site}.com/ ${defaultUser} `,
+                expected: defaultUser,
+            });
+            break;
+        case YOUTUBE_ONLY_TEST_TYPES.CHANNEL_FULL:
+            tests.push({
+                arg: `https://www.youtube.com/channel/${defaultUser}`,
+                expected: defaultUser,
+            });
+            tests.push({
+                arg: `http://www.youtube.com/channel/${defaultUser}`,
+                expected: defaultUser,
+            });
+            tests.push({
+                arg: `www.youtube.com/channel/${defaultUser}`,
+                expected: defaultUser,
+            });
+            tests.push({
+                arg: `youtube.com/channel/${defaultUser}`,
+                expected: defaultUser,
+            });
+            break;
+        case YOUTUBE_ONLY_TEST_TYPES.CHANNEL_SHORTHAND:
+            tests.push({
+                arg: `https://www.youtube.com/c/${defaultUser}`,
+                expected: defaultUser,
+            });
+            tests.push({
+                arg: `http://www.youtube.com/c/${defaultUser}`,
+                expected: defaultUser,
+            });
+            tests.push({
+                arg: `www.youtube.com/c/${defaultUser}`,
+                expected: defaultUser,
+            });
+            tests.push({
+                arg: `youtube.com/c/${defaultUser}`,
+                expected: defaultUser,
+            });
+            break;
+        case YOUTUBE_ONLY_TEST_TYPES.LEGACY_USER:
+            tests.push({
+                arg: `https://www.youtube.com/user/${defaultUser}`,
+                expected: defaultUser,
+            });
+            tests.push({
+                arg: `http://www.youtube.com/user/${defaultUser}`,
+                expected: defaultUser,
+            });
+            tests.push({
+                arg: `www.youtube.com/user/${defaultUser}`,
+                expected: defaultUser,
+            });
+            tests.push({
+                arg: `youtube.com/user/${defaultUser}`,
                 expected: defaultUser,
             });
             break;
