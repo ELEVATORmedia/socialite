422013e3d37ebc434175344e6185457a76e5fdfb
Jesse Meza <jmeza081@gmail.com>
2020-02-05 14:20:36 -0600

    add youtube tests

--- test/frenchPress.spec.js
+++ test/frenchPress.spec.js
@@ -1,15 +1,15 @@
-const { TEST_TYPES } = require('./enums');
+const { STANDARD_TEST_TYPES, YOUTUBE_ONLY_TEST_TYPES } = require('./enums');
 const assert = require('assert');
 const frenchPress = require('../src');
 const testUtils = require('./utils');
 
 describe('frenchPress.extractUsername(url)', () => {
     describe('FACEBOOK urls', () => {
-        Object.keys(TEST_TYPES).map((key) => {
-            describe(TEST_TYPES[key], () => {
+        Object.keys(STANDARD_TEST_TYPES).map((key) => {
+            describe(STANDARD_TEST_TYPES[key], () => {
                 var testCases = testUtils.getExtractUsernameTestCase(
                     'facebook',
-                    TEST_TYPES[key],
+                    STANDARD_TEST_TYPES[key],
                 );
 
                 testCases.forEach((test) => {
@@ -24,11 +24,11 @@ describe('frenchPress.extractUsername(url)', () => {
     });
 
     describe('INSTRAGRAM urls', () => {
-        Object.keys(TEST_TYPES).map((key) => {
-            describe(TEST_TYPES[key], () => {
+        Object.keys(STANDARD_TEST_TYPES).map((key) => {
+            describe(STANDARD_TEST_TYPES[key], () => {
                 var testCases = testUtils.getExtractUsernameTestCase(
                     'instragram',
-                    TEST_TYPES[key],
+                    STANDARD_TEST_TYPES[key],
                 );
 
                 testCases.forEach((test) => {
@@ -43,11 +43,11 @@ describe('frenchPress.extractUsername(url)', () => {
     });
 
     describe('TWITTER urls', () => {
-        Object.keys(TEST_TYPES).map((key) => {
-            describe(TEST_TYPES[key], () => {
+        Object.keys(STANDARD_TEST_TYPES).map((key) => {
+            describe(STANDARD_TEST_TYPES[key], () => {
                 var testCases = testUtils.getExtractUsernameTestCase(
                     'twitter',
-                    TEST_TYPES[key],
+                    STANDARD_TEST_TYPES[key],
                 );
 
                 testCases.forEach((test) => {
@@ -62,11 +62,11 @@ describe('frenchPress.extractUsername(url)', () => {
     });
 
     describe('SOUNDCLOUD urls', () => {
-        Object.keys(TEST_TYPES).map((key) => {
-            describe(TEST_TYPES[key], () => {
+        Object.keys(STANDARD_TEST_TYPES).map((key) => {
+            describe(STANDARD_TEST_TYPES[key], () => {
                 var testCases = testUtils.getExtractUsernameTestCase(
                     'soundcloud',
-                    TEST_TYPES[key],
+                    STANDARD_TEST_TYPES[key],
                 );
 
                 testCases.forEach((test) => {
@@ -81,11 +81,30 @@ describe('frenchPress.extractUsername(url)', () => {
     });
 
     describe('YOUTUBE standard urls ("youtube.com/username")', () => {
-        Object.keys(TEST_TYPES).map((key) => {
-            describe(TEST_TYPES[key], () => {
+        Object.keys(STANDARD_TEST_TYPES).map((key) => {
+            describe(STANDARD_TEST_TYPES[key], () => {
                 var testCases = testUtils.getExtractUsernameTestCase(
                     'youtube',
-                    TEST_TYPES[key],
+                    STANDARD_TEST_TYPES[key],
+                );
+
+                testCases.forEach((test) => {
+                    it(testUtils.getDescription(test), () => {
+                        const extractedUsername = frenchPress.extractUsername(test.arg);
+
+                        assert.equal(extractedUsername, test.expected);
+                    });
+                });
+            });
+        });
+    });
+
+    describe('YOUTUBE variant urls ("youtube.com/c/", "youtube.com/channel", "youtube.com/user/)', () => {
+        Object.keys(YOUTUBE_ONLY_TEST_TYPES).map((key) => {
+            describe(YOUTUBE_ONLY_TEST_TYPES[key], () => {
+                var testCases = testUtils.getExtractUsernameTestCase(
+                    'youtube',
+                    YOUTUBE_ONLY_TEST_TYPES[key],
                 );
 
                 testCases.forEach((test) => {
