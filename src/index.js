f1f75becab3895f1abe7f5cb671b797c49eed441
Jesse Meza <jmeza081@gmail.com>
2020-02-05 15:27:49 -0600

    add unbrew and build regex

--- src/index.js
+++ src/index.js
@@ -3,11 +3,12 @@
  * These urls are typically formatted as https://www.socialMediaSiteDomain.com/username
  */
 const STANDARD_SOCIAL_DOMAINS = ['facebook', 'instagram', 'twitter', 'soundcloud'];
+const SUPPORTED_SOCIAL_DOMAINS = ['youtube', ...STANDARD_SOCIAL_DOMAINS];
 
 /**
  * Iteratively builds the final regular expression to be used in the replace operation.
  */
-const buildRegex = () => {
+const getRegex = () => {
     // Matches https:// or http:// if available
     // eslint-disable-next-line no-useless-escape
     const optionalProtocol = `(https?:\/\/)?`;
@@ -46,13 +47,62 @@ const buildRegex = () => {
 };
 
 /**
+ * Returns a template-filled string based on the batter
+ * https://www.socialMediaSite.com/username
+ */
+const buildStandardURL = (domain, username) => {
+    return `https://www.${domain}.com/${username}`;
+};
+
+/**
+ * Interpolates the type of username to figure out which algorithm
+ * to use in returning the absolute URL. if the username begins with
+ * 'UC' or 'HC', we use a channel template string, otherwise we
+ * default to the standard URL pattern
+ */
+const buildYoutubeVariantURL = (username) => {
+    if (username.startsWith('UH') || username.startsWith('HC')) {
+        return `https://www.youtube.com/c/${username}`;
+    } else {
+        return buildStandardURL('youtube', username);
+    }
+};
+
+/**
+ * Attempts to build an absolute url from a username and its mapped type for
+ * a social media. for example, if given 'username' and 'facebook' the output
+ * will be 'https://www.facebook.com/username'
+ */
+const unbrew = (username, type) => {
+    if (!username) {
+        return '';
+    }
+
+    if (!SUPPORTED_SOCIAL_DOMAINS.includes(type)) {
+        return '';
+    }
+
+    switch (type) {
+        case SUPPORTED_SOCIAL_DOMAINS.FACEBOOK:
+        case SUPPORTED_SOCIAL_DOMAINS.INSTAGRAM:
+        case SUPPORTED_SOCIAL_DOMAINS.TWITTER:
+        case SUPPORTED_SOCIAL_DOMAINS.SOUNDCLOUD:
+            return buildStandardURL(username, type);
+
+        case SUPPORTED_SOCIAL_DOMAINS.YOUTUBE:
+            return buildYoutubeVariantURL(username);
+    }
+};
+
+/**
  * Attempts to extract the username from a social media url by parsing out
  * social media url prefixes and blacklisted characters
  */
-const extractUsername = (url) => {
-    const socialRegex = buildRegex();
+const plunge = (url) => {
+    const socialRegex = getRegex();
     return url.replace(socialRegex, '');
 };
 
-exports.extractUsername = extractUsername;
-exports.buildRegex = buildRegex;
+exports.plunge = plunge;
+exports.getRegex = getRegex;
+exports.unbrew = unbrew;
