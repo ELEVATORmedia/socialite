f8cdf1840082e4ec735856a0ddf2d25ce9bcbdab
Jesse Meza <jmeza081@gmail.com>
2020-02-06 16:02:17 -0600

    add separate tests for multiple ops

--- test/frenchPress.spec.js
+++ test/frenchPress.spec.js
@@ -1,10 +1,10 @@
 const { STANDARD_TEST_TYPES, YOUTUBE_ONLY_TEST_TYPES } = require('./test-enums');
 const { ALL_SOCIAL_DOMAINS } = require('../src/enums');
 const assert = require('assert');
-const frenchPress = require('../src');
+const frenchPress = require('../src/frenchPress');
 const testUtils = require('./utils');
 
-describe('frenchPress.plunge(url)', () => {
+describe('frenchPress.plunge(url, singleOperation=false)', () => {
     describe('FACEBOOK urls', () => {
         Object.keys(STANDARD_TEST_TYPES).forEach((key) => {
             describe(STANDARD_TEST_TYPES[key], () => {
@@ -111,6 +111,122 @@ describe('frenchPress.plunge(url)', () => {
                 testCases.forEach((test) => {
                     it(testUtils.getDescription(test), () => {
                         const extractedUsername = frenchPress.plunge(test.arg);
+
+                        assert.equal(extractedUsername, test.expected);
+                    });
+                });
+            });
+        });
+    });
+});
+
+describe('frenchPress.plunge(url, singleOperation=true)', () => {
+    describe('FACEBOOK urls', () => {
+        Object.keys(STANDARD_TEST_TYPES).forEach((key) => {
+            describe(STANDARD_TEST_TYPES[key], () => {
+                var testCases = testUtils.getExtractUsernameTestCase(
+                    'facebook',
+                    STANDARD_TEST_TYPES[key],
+                );
+
+                testCases.forEach((test) => {
+                    it(testUtils.getDescription(test), () => {
+                        const extractedUsername = frenchPress.plunge(test.arg, true);
+
+                        assert.equal(extractedUsername, test.expected);
+                    });
+                });
+            });
+        });
+    });
+
+    describe('INSTAGRAM urls', () => {
+        Object.keys(STANDARD_TEST_TYPES).forEach((key) => {
+            describe(STANDARD_TEST_TYPES[key], () => {
+                var testCases = testUtils.getExtractUsernameTestCase(
+                    'instagram',
+                    STANDARD_TEST_TYPES[key],
+                );
+
+                testCases.forEach((test) => {
+                    it(testUtils.getDescription(test), () => {
+                        const extractedUsername = frenchPress.plunge(test.arg, true);
+
+                        assert.equal(extractedUsername, test.expected);
+                    });
+                });
+            });
+        });
+    });
+
+    describe('TWITTER urls', () => {
+        Object.keys(STANDARD_TEST_TYPES).forEach((key) => {
+            describe(STANDARD_TEST_TYPES[key], () => {
+                var testCases = testUtils.getExtractUsernameTestCase(
+                    'twitter',
+                    STANDARD_TEST_TYPES[key],
+                );
+
+                testCases.forEach((test) => {
+                    it(testUtils.getDescription(test), () => {
+                        const extractedUsername = frenchPress.plunge(test.arg, true);
+
+                        assert.equal(extractedUsername, test.expected);
+                    });
+                });
+            });
+        });
+    });
+
+    describe('SOUNDCLOUD urls', () => {
+        Object.keys(STANDARD_TEST_TYPES).forEach((key) => {
+            describe(STANDARD_TEST_TYPES[key], () => {
+                var testCases = testUtils.getExtractUsernameTestCase(
+                    'soundcloud',
+                    STANDARD_TEST_TYPES[key],
+                );
+
+                testCases.forEach((test) => {
+                    it(testUtils.getDescription(test), () => {
+                        const extractedUsername = frenchPress.plunge(test.arg, true);
+
+                        assert.equal(extractedUsername, test.expected);
+                    });
+                });
+            });
+        });
+    });
+
+    describe('YOUTUBE standard urls ("youtube.com/username")', () => {
+        Object.keys(STANDARD_TEST_TYPES).forEach((key) => {
+            describe(STANDARD_TEST_TYPES[key], () => {
+                var testCases = testUtils.getExtractUsernameTestCase(
+                    'youtube',
+                    STANDARD_TEST_TYPES[key],
+                );
+
+                testCases.forEach((test) => {
+                    it(testUtils.getDescription(test), () => {
+                        const extractedUsername = frenchPress.plunge(test.arg, true);
+
+                        assert.equal(extractedUsername, test.expected);
+                    });
+                });
+            });
+        });
+    });
+
+    describe('YOUTUBE variant urls ("youtube.com/c/", "youtube.com/channel", "youtube.com/user/)', () => {
+        Object.keys(YOUTUBE_ONLY_TEST_TYPES).forEach((key) => {
+            describe(YOUTUBE_ONLY_TEST_TYPES[key], () => {
+                var testCases = testUtils.getExtractUsernameTestCase(
+                    'youtube',
+                    YOUTUBE_ONLY_TEST_TYPES[key],
+                );
+
+                testCases.forEach((test) => {
+                    it(testUtils.getDescription(test), () => {
+                        const extractedUsername = frenchPress.plunge(test.arg, true);
 
                         assert.equal(extractedUsername, test.expected);
                     });
